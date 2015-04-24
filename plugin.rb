efb6fbaf9804cf68682ebe52ce614358c53cc39f
cpradio <forums@cpradio.net>
2015-04-24 13:13:41 -0400

    Added url comment

--- plugin.rb
+++ plugin.rb
@@ -2,6 +2,7 @@
 # about: adds the old vb emojis
 # version: 0.1
 # authors: Matthew Wilkin and Allan
+# url: https://github.com/cpradio/vb_emoji
 
 register_asset "javascripts/vb-emoji.js", :server_side
 register_asset "stylesheets/vb-emoji.css"
